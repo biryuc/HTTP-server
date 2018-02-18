db2af5c54c02316e840f16f9cd7f8295b8d3b173
Parker Diamond <jparkerdiamond@gmail.com>
2018-02-18 02:02:28 -0500

    Fixed bugs on handing back response; added some header parsing

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import sys
 class HTTP_server():
 
     host = '127.0.0.1'
-    port = 8080
+    port = 8081
     buf_size = 1024
     max_conn = 200
 
@@ -47,19 +47,33 @@ Content-Type: text/html
     def handle_client(self, connection, address):
         # accept an incoming client connection and parse the input data stream into a HTTP request
         connection.setblocking(False)
-        inputs,outputs,errors = select.select([connection],[connection],[])
 
-        message = ''
-        for client_input in inputs:
-            data = connection.recv(self.buf_size)
-            message += str(data)
+        inputs,outputs,errors = select.select([connection],[],[])
+
+        # Read from socket until input is exhausted
+        if inputs:
+            request = ''
+            while True:
+                try:
+                    request += str(connection.recv(self.buf_size))
+                except Exception as exp:
+                    print(request)
+                    break
             
-        print(message)
-
+            response = self.parse_request(request)
+            connection.sendall(response)
+            connection.close()
         
-        # Based on the request's parameters form a response and send it back to the client
-        connection.sendall(self.response)
-        #connection.close()
+    def parse_request(self, request):
+        fields = request.split('\n')[0].split(' ')
+        if fields[0] == 'GET':
+            resource = fields[1]
+            print(resource)
+        else:
+            response = b'HTTP/1.0 501 Not Implemented\n'
+            
+        return self.response
+        pass
 
         # the server continues to perform steps 2 (GET) and 3 (dir list) for as long as the server is running
         # if in multi-thread mode, then fork a thread after we accept a connection
@@ -67,7 +81,7 @@ Content-Type: text/html
 
 if __name__ == "__main__":
     #Accept a flag '--threaded' to make the server threaded
-    if sys.argv[1] == '--threaded':
+    if len(sys.argv) >= 1 and sys.argv[1] == '--threaded':
         http = HTTP_server(threaded=True)
     else:
         http = HTTP_server()
