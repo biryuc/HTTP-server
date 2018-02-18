bb5d0938b21a52dbf0e5c9329341ac6d3104a11d
Parker Diamond <jparkerdiamond@gmail.com>
2018-02-17 23:34:15 -0500

    Added threading to handling responses; properly formatted HTML response

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
+import select
 import socket
+import threading
 import sys
 
 class HTTP_server():
 
-    host = ''
-    port = 10010
+    host = '127.0.0.1'
+    port = 8080
     buf_size = 1024
+    max_conn = 200
 
     # temporary response message for testing
 
     response = b"""\
 HTTP/1.0 200 OK
-
 Content-Type: text/html
 
+<!DOCTYPE html>
 <html>
     <head>
         <title>Test Page</title>
@@ -26,29 +29,46 @@ Content-Type: text/html
 </html>
 """
 
-    def __init__(self):
+    def __init__(self,threaded=False):
 
         # allocate a server socket, bind it to a port, and then listen for incoming connections
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((self.host, self.port))
-        self.s.listen(1)
 
-    def handle_client(self):
-        while 1:
-            conn, address = self.s.accept()
+        if threaded:
+            self.s.listen(self.max_conn)
+        else:
+            self.s.listen(1)
 
-            # accept an incoming client connection and parse the input data stream into a HTTP request
-            request_data = conn.recv(self.buf_size)
-            print(request_data)
+        while True:
+            threading.Thread(target=self.handle_client, args=(self.s.accept())).start()
 
-            # Based on the request's parameters form a response and send it back to the client
-            conn.sendall(self.response)
-            conn.close()
+    def handle_client(self, connection, address):
+        # accept an incoming client connection and parse the input data stream into a HTTP request
+        connection.setblocking(False)
+        inputs,outputs,errors = select.select([connection],[connection],[])
 
-            # the server continues to perform steps 2 (GET) and 3 (dir list) for as long as the server is running
-            # if in multi-thread mode, then fork a thread after we accept a connection
-            # and let the child thread handle parsing and responding to the request
+        message = ''
+        for client_input in inputs:
+            data = connection.recv(self.buf_size)
+            message += str(data)
+            
+        print(message)
 
-http = HTTP_server()
-http.handle_client()
+        
+        # Based on the request's parameters form a response and send it back to the client
+        connection.sendall(self.response)
+        #connection.close()
+
+        # the server continues to perform steps 2 (GET) and 3 (dir list) for as long as the server is running
+        # if in multi-thread mode, then fork a thread after we accept a connection
+        # and let the child thread handle parsing and responding to the request
+
+if __name__ == "__main__":
+    #Accept a flag '--threaded' to make the server threaded
+    if sys.argv[1] == '--threaded':
+        http = HTTP_server(threaded=True)
+    else:
+        http = HTTP_server()
+
